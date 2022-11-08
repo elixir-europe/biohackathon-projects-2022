122b4eda5ecb6b96ef6547b756b95474e3e00cbc
blacruz17 <76912800+blacruz17@users.noreply.github.com>
2022-11-08 17:48:52 +0100

    run several queries

--- 1/workflow/example_code.R
+++ 1/workflow/example_code.R
@@ -1,55 +1,84 @@
 #get required packages
 library('httr')
 library('jsonlite')
+library('tidyverse')
 
-# Documentation:
+# ZOOMA API documentation:
 # https://www.ebi.ac.uk/spot/zooma/docs/api
 
-# now let's try the same thing with several items
-unique_list_df <- read_csv('./1/workflow/unique_list_df.csv')
-
-unique_list_df %>% URLencode(reserved = TRUE)
 #adding links
 base <- "www.ebi.ac.uk/spot/zooma/v2/api"
 endpoint <- "/services/annotate"
 
-inputTerm <- "Honey, raw" # rows from input dataframe
-food <- inputTerm %>% URLencode(reserved = TRUE)
+# load the (example) list of unique items:
+unique_list_df <- read_csv('./1/workflow/unique_list_df.csv')
 
-call1 <- paste(base,endpoint,"?","propertyValue","=", food, sep="")
-call1
+# we need to URL-encode the terms:
+unique_list_df$input <- apply(unique_list_df, 1, 
+                              function(x) URLencode(x, reserved = TRUE))
+
+# build all the calls:
+unique_list_df <- unique_list_df %>%
+  mutate(call = paste0(base, endpoint, "?propertyValue=", input))
 
 # if we want to limit the sources & ontologies:
-required_sources <- c('ebisc', 'uniprot') # this could be set by the user
+# (this could be set by the user)
+required_sources <- c() 
 required_ontologies <- c('foodon')
 
 # we need this to be formatted in a specific way:
+if (length(required_sources) > 0){
+  format_sources <- paste0('[', paste(required_sources, collapse = ','), ']')
+  } else {
+  format_sources <- '[none]'}
 
-format_sources <- paste0('[', paste(required_sources, collapse = ','), ']')
-format_ontologies <- paste0('[', paste(required_ontologies, collapse = ','), ']')
+if (length(required_ontologies) > 0){
+  format_ontologies <- paste0('[', paste(required_ontologies, collapse = ','), ']')
+  } else {
+  format_ontologies <- '[none]'}
 
+# now we can build our filtered calls:
+unique_list_df <- unique_list_df %>%
+  mutate(call_filters = paste0(call, 
+                              '&filter=', 
+                              'required:', format_sources, ',',
+                              'ontologies:', format_ontologies))
 
-url_filters <- paste0('&filter=', 
-                      'required:', format_sources, ',',
-                      'ontologies:', format_ontologies)
+# prepare an empty dataframe for results:
+results_df <- data.frame(inputTerm=character(),
+                         semanticTags=character(),
+                         confidence=character(),
+                         annotatedProperty.propertyValue=character(),
+                         derivedFrom.provenance.source.type=character(),
+                         stringsAsFactors=FALSE)
 
-call1 <- paste0(call1, url_filters)
+# loop over all the terms and query the ZOOMA API:
+# this is a bit slow - can we improve it?
+for (i in 1:dim(unique_list_df)[1]){
+  # use the call with filters:
+  res <- GET(url = unique_list_df$call_filters[i])
+  
+  res_text <- content(res, 'text')
+  res_json <- fromJSON(res_text, flatten = TRUE)
+  res_df <- as.data.frame(res_json)
+  
+  # we need to avoid empty results somehow:
+  if (dim(res_df)[1] > 0){
+  summary_res <- res_df[ , c('semanticTags',
+                                 'confidence',
+                                 'annotatedProperty.propertyValue',
+                                 'derivedFrom.provenance.source.type')]
+  
+  # add the original search term:
+  summary_res$searchTerm <- unique_list_df$List[i]
+  
+  # done! merge it with the previous results:
+  results_df <- rbind(results_df, summary_res)}
+}
 
+# let's relocate the search term column to the 1st place
+# to make the data a bit more understandable for us:
+results_df <- results_df %>% relocate(searchTerm)
 
-r <- GET(url = call1)
-
-food_text <- content(r, 'text')
-
-food_json <- fromJSON(food_text, flatten = TRUE)
-
-food_df <- as.data.frame(food_json)
-
-
-View(food_df)
-
-summary_result <- food_df[ , c('semanticTags',
-                               'confidence',
-                               'annotatedProperty.propertyValue',
-                               'derivedFrom.provenance.source.type')]
-
-summary_result$searchTerm <- inputTerm
+# done! we can take a look...
+View(results_df)
