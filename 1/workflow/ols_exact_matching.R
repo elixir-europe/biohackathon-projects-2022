ee9274d5857013c45e5307309839c878fa6b99be
blacruz17 <76912800+blacruz17@users.noreply.github.com>
2022-11-10 17:56:49 +0100

    implement ontology selection before search

--- 1/workflow/ols_exact_matching.R
+++ 1/workflow/ols_exact_matching.R
@@ -7,20 +7,35 @@ library('glue')
 base <- "http://www.ebi.ac.uk/ols/api/"
 endpoint <- "search"
 
+# user input to restrict the search to some ontologies:
+limit_ontologies <- c("foodon", "fobi") # for example
+
+# the complete list of ontologies can be accessed at:
+r_ontol <- GET(paste0(base, 'ontologies'))
+r_ontol_text <- content(r_ontol, 'text')
+r_ontol_json <- fromJSON(r_ontol_text, flatten = TRUE)
+ontologies <- r_ontol_json[['_embedded']][['ontologies']]
+
+
 # let's use the same example:
 unique_list_df <- read_csv('./1/workflow/unique_list_df.csv') %>%
                     rename(inputTerm = List)
 
 # URL-encode and build the queries for exact matching:
-## we can add: "&groupField=TRUE" as a parameter to get 
-## exactly the same results as with the webpage:
+## NOTE: we can add "&groupField=TRUE" as a parameter to get 
+## exactly the same results as with the webpage
 search_table <- unique_list_df %>%
   mutate(query = URLencode(inputTerm, reserved = TRUE)) %>%
-  mutate(query = as.character(glue("{base}{endpoint}?q={query}&exact=TRUE"))) %>% 
-  mutate(query_group = as.character(glue("{query}&groupField=TRUE")))
+  mutate(query = as.character(glue("{base}{endpoint}?q={query}&exact=TRUE")))# %>% 
+  # mutate(query = as.character(glue("{query}&groupField=TRUE")))
+
+# add parameter to limit the results to some ontologies:
+if (length(limit_ontologies) > 0) {
+  search_table <- search_table %>%
+    mutate(query = as.character(glue("{query}&ontology={paste(limit_ontologies, collapse = ',')}")))
+  }
 
 # now get all the results:
-# TODO: limit ontologies
 results <- search_table %>% 
   mutate(result = map(query, ~ GET(url = ..1))) %>% 
   mutate(result = map(result, ~ content(..1, 'text'))) %>%
@@ -30,25 +45,6 @@ results <- search_table %>%
 
 final_results <- results %>% 
                   unnest(result) %>%
-                  select(inputTerm, iri, obo_id, label,
-                         ontology_name, description)
+                  select(inputTerm, iri, obo_id, label, 
+                         ontology_name)
 View(final_results)
-
-## another possibility - grouped results:
-results_group <- search_table %>% 
-  mutate(result = map(query_group, ~ GET(url = ..1))) %>% 
-  mutate(result = map(result, ~ content(..1, 'text'))) %>%
-  mutate(result = map(result, ~ fromJSON(..1, flatten = TRUE))) %>% 
-  mutate(result = map(result, ~ as_tibble(..1[["response"]][["docs"]])))
-
-
-final_results_group <- results_group %>% 
-  unnest(result) %>%
-  select(inputTerm, iri, obo_id, label,
-         ontology_name, description)
-
-View(final_results_group)
-
-# what's the difference?:
-setdiff(final_results, final_results_group)
-
